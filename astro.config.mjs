bbeeaddf916fe89194842c8ea7f62fa29b532762
Josh Rauvola <jrauvola@Joshs-MBP.lan>
2025-04-16 04:26:17 -0500

    Site Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://jrauvola.github.io',
+  base: '/awesome-green-software',
   integrations: [react(), tailwind({
     applyBaseStyles: false
   })]
