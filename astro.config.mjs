4f48dc2e9b4020ed08495ab321eddcb0e97eacc3
Josh Rauvola <jrauvola@Mac.lan>
2025-10-30 22:34:27 -0500

    Default base path for GitHub Pages builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+const isProductionBuild = process.env.NODE_ENV === 'production';
+const defaultBase = isProductionBuild ? '/awesome-green-software' : '/';
+const base = process.env.BASE_PATH ?? defaultBase;
+const site = process.env.SITE_URL ?? (base !== '/' ? `https://green-software-foundation.github.io${base}` : undefined);
+
 export default defineConfig({
-  site: process.env.SITE_URL || undefined,
-  base: process.env.BASE_PATH || '/',
+  site,
+  base,
   integrations: [react(), tailwind({
     applyBaseStyles: true
   })],
